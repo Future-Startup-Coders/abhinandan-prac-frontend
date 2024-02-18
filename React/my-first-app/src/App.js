87c2d567fd52a0ab4e62b1b17a9ef0edd9ccfde1
A3ANSHU <abhinandanambekar@gmail.com>
2024-02-19 01:12:53 +0530

    my-first-app

---
+++ React/my-first-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   <div className='abhi'>
+    <h1>ABHINANDAN AMBEKAR</h1>
+    <h2>hello i am a <span>devloper</span></h2>
+   </div>
+  );
+}
+
+export default App;
